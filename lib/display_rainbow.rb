74f6b5a07c666da0bf516d1ab8b58096fd4525c7
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-29 17:39:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow (colors)
-  puts "(R: red, O: orange, Y: yeloow, G: green, B: blue, I: indigo, V: violet)"
+  puts "#{ R: red, O: orange, Y: yeloow, G: green, B: blue, I: indigo, V: violet"}"
 end
