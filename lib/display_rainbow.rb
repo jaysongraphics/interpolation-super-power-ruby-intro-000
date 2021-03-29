335d6e2335a788b753f882de9ad8f3511ed764c5
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-29 15:16:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(color)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "color: #{R[red]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
